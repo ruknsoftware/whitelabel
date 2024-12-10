4852ae0166f9c2c0c317b4e7e80472ddcce58832
HAMZA ABUABADA <abadahhamza@gmail.com>
2024-12-10 18:08:15 +0200

    feat(Kanaan): add Kanaan image (#1)
    * feat(Kanaan): add Kanaan image
    
    * feat(Hooks): change logo name
    
    * feat(init): change logo name
    
    * feat(App Name): set app name after install is Kanaan
    
    * feat(init): update version

--- whitelabel/__init__.py
+++ whitelabel/__init__.py
@@ -2,9 +2,9 @@
 from __future__ import unicode_literals
 import frappe
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 if frappe.conf and frappe.conf.get("app_logo_url"):
-    __logo__ = frappe.conf.get("app_logo_url") or '/assets/whitelabel/images/whitelabel_logo.jpg'
+    __logo__ = frappe.conf.get("app_logo_url") or '/assets/whitelabel/images/Kanaan_Logo.png'
 else:
-    __logo__ = '/assets/whitelabel/images/whitelabel_logo.jpg'
+    __logo__ = '/assets/whitelabel/images/Kanaan_Logo.png'
--- whitelabel/hooks.py
+++ whitelabel/hooks.py
@@ -12,7 +12,7 @@ app_icon = "octicon octicon-file-directory"
 app_color = "grey"
 app_email = "maheshwaribhavesh95863@gmail.com"
 app_license = "MIT"
-app_logo_url = '/assets/whitelabel/images/whitelabel_logo.jpg'
+app_logo_url = '/assets/whitelabel/images/Kanaan_Logo.png'
 
 # Includes in <head>
 # ------------------
@@ -49,8 +49,8 @@ web_include_css = "/assets/whitelabel/css/whitelabel_web.css"
 # get_website_user_home_page = "whitelabel.utils.get_home_page"
 
 website_context = {
-	"favicon": app_logo or "/assets/whitelabel/images/whitelabel_logo.jpg",
-	"splash_image": app_logo or "/assets/whitelabel/images/whitelabel_logo.jpg"
+	"favicon": app_logo or "/assets/whitelabel/images/Kanaan_Logo.png",
+	"splash_image": app_logo or "/assets/whitelabel/images/Kanaan_Logo.png"
 }
 after_migrate = ['whitelabel.api.whitelabel_patch']
 
@@ -64,7 +64,7 @@ after_migrate = ['whitelabel.api.whitelabel_patch']
 # ------------
 
 # before_install = "whitelabel.install.before_install"
-# after_install = "whitelabel.install.after_install"
+after_install = "whitelabel.install.set_app_name"
 
 # Desk Notifications
 # ------------------
