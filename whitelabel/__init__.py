6cbc02b079e4323b381c53b451fb223137e7df97
Husam Hammad <85282854+husamhammad@users.noreply.github.com>
2024-12-17 12:04:45 +0200

    feat:attachment logo into whitelabel settings doctype in version 14 (#7)
    * feat:attachment logo into whitelabel settings doctype in version 14
    
    * feat(__init__):Version Update

--- whitelabel/__init__.py
+++ whitelabel/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 import frappe
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 if frappe.conf and frappe.conf.get("app_logo_url"):
     __logo__ = frappe.conf.get("app_logo_url") or '/assets/whitelabel/images/Kanaan_Logo.png'
